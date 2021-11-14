2309b9755aaf5e3c2d892da41f22ed1e832ed8b4
khaliljedda <khaliljedda4@gmail.com>
2021-11-14 11:01:54 +0100

    blog design

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-    <div id="app">
-      <h1 class="m-auto">simple blog app</h1>
-      <b-card v-for="post in posts" :key="post.key">
-        <h2>{{ post.name }}</h2>
-        <div class="col-6 m-auto">
-          <p>{{ post.description }}t</p>
-        </div>
+   <div class="header">
+  <h2>Simple Test Blog </h2>
+</div>
 
-        <br />
-      </b-card>
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card" v-for="post in posts" :key="post.key">
+      <h2>{{post.name}}</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>{{post.description}}</p>
     </div>
+    
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
   </div>
 </template>
 
@@ -45,6 +59,75 @@
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+/* Add a gray background color with some padding */
+body {
+  font-family: Arial;
+  padding: 20px;
+  background: #f1f1f1;
+}
+
+/* Header/Blog Title */
+.header {
+  padding: 30px;
+  font-size: 40px;
+  text-align: center;
+  background: white;
+}
+
+/* Create two unequal columns that floats next to each other */
+/* Left column */
+.leftcolumn {   
+  float: left;
+  width: 75%;
+}
+
+/* Right column */
+.rightcolumn {
+  float: left;
+  width: 25%;
+  padding-left: 20px;
+}
+
+/* Fake image */
+.fakeimg {
+  background-color: #aaa;
+  width: 100%;
+  padding: 20px;
+}
+
+/* Add a card effect for articles */
+.card {
+   background-color: white;
+   padding: 20px;
+   margin-top: 20px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Footer */
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #ddd;
+  margin-top: 20px;
+}
+
+/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 800px) {
+  .leftcolumn, .rightcolumn {   
+    width: 100%;
+    padding: 0;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
