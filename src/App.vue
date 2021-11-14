9121fa6f0d49138992ba58545638e2ed829aa845
khaliljedda <khaliljedda4@gmail.com>
2021-11-14 12:28:27 +0100

    blog desgin 2

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,29 @@
     <div class="card" v-for="post in posts" :key="post.key">
       <h2>{{post.name}}</h2>
       <h5>Title description, Dec 7, 2017</h5>
-      <div class="fakeimg" style="height:200px;">Image</div>
+     <img src="blog.png" style="height:200px;" alt="">
       <p>Some text..</p>
       <p>{{post.description}}</p>
     </div>
+
     
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>About Me</h2>
+      <div class="fakeimg" style="height:100px;">Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    </div>
+    <div class="card">
+      <h3>Popular Post</h3>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div>
+    </div>
     <div class="card">
       <h3>Follow Me</h3>
       <p>Some text..</p>
